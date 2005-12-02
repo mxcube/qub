7d3127ebb4eb8a0ce1e3be47efad0d0301230047
berruyer <berruyer@6dc7f912-7f33-0410-92c5-e0fef42ab755>
2005-12-02 07:13:09 +0000

    + QubAction contain only base class for actions (QubAction, QubImageAction, QubToggleImageAction)
    + change place of import for test
    
    
    git-svn-id: https://scm.blissgarden.org/svn/qub/trunk@40 6dc7f912-7f33-0410-92c5-e0fef42ab755

--- Widget/QubAction.py
+++ Widget/QubAction.py
@@ -170,13 +170,244 @@ class QubAction(qt.QObject):
         the desired action.
         """
         pass
+        
+################################################################################
+####################              QubImageAction            ####################
+################################################################################
+class QubImageAction(QubAction):
+    """
+    This inherits QubAction and add empty method for action acting on
+    QubImage Object.
+    Main adds on are slots for drawings
+    """
+    def __init__(self, name=None, qubImage=None, place="toolbar",
+                 show=1, group="", index=-1):
+        """
+        Constructor method
+        name ... :  string name of the action
+        qubImage :  action will act on a QubImage object. It can be set in
+                    the constructor method or using the "viewConnect" method
+                    of the class 
+        place .. :  where to put in the view widget, the selection widget
+                    of the action ("toolbar", "statusbar", None)
+        show ... :  If in view toolbar, tells to put it in the toolbar
+                    itself or in the context menu
+        group .. :  actions may grouped. Tells the name of the group the
+                    action belongs to. If not present, a "misc." group is
+                    automatically created and the action is added to it
+        index .. :  Position of the selection widget of the action in its
+                    group
+        """
+        QubAction.__init__(self, name, place, show, group, index)
+        
+        self._qubImage = None
+        
+        if qubImage is not None:
+            self.viewConnect(qubImage)
+
+    def viewConnect(self, qubImage):
+        """
+        Register the "qubImage" object
+        Connect action to "qubImage" signals:
+            "ForegroundColorChanged" : Foreground color has been changed
+            "MouseMoved" : Mouse has been moved inside the viewport of the
+                           "qubImage"
+            "MousePressed" : Mouse has been pressed inside he viewport of the
+                             "qubImage"
+            "MouseReleased" : Mouse has been released inside he viewport of
+                              the "qubImage"
+            "ViewportUpdated" : displayed pixmap of "qubImage" has changed 
+                                (new pixmap or new size)
+        """
+        if self._qubImage is not None and qubImage is not None and \
+           self._qubImage != qubImage:
+            """
+            disconnect action from previous qubImage
+            """
+            self._disconnect(self._qubImage)
+            self._qubImage = None
+
+        if qubImage is not None:
+            self._qubImage = qubImage
+            self._connect(self._qubImage)
+            
+    def _connect(self, view):
+        """
+        Connect action to view signals
+        """
+        self.connect(view, qt.PYSIGNAL("ForegroundColorChanged"),
+                        self.setColor)
+        self.connect(view, qt.PYSIGNAL("MouseMoved"),
+                        self.mouseMove)
+        self.connect(view, qt.PYSIGNAL("MousePressed"),
+                        self.mousePress)
+        self.connect(view, qt.PYSIGNAL("MouseReleased"),
+                        self.mouseRelease)
+        self.connect(view, qt.PYSIGNAL("ViewportUpdated"),
+                        self.viewportUpdate)
+    
+    def _disconnect(self, view):
+        """
+        Disconnect action to view signals
+        """
+        self.disconnect(view, qt.PYSIGNAL("ForegroundColorChanged"),
+                        self.setColor)
+        self.disconnect(view, qt.PYSIGNAL("MouseMoved"),
+                        self.mouseMove)
+        self.disconnect(view, qt.PYSIGNAL("MousePressed"),
+                        self.mousePress)
+        self.disconnect(view, qt.PYSIGNAL("MouseReleased"),
+                        self.mouseRelease)
+        self.disconnect(view, qt.PYSIGNAL("ViewportUpdated"),
+                        self.viewportUpdate)
+                            
+    def setColor(self, color):
+        """
+        Slot connected to "ForegroundColorChanged" "qubImage" signal
+        To be reimplemented.
+        """
+        pass
+        
+    def mouseMove(self, event):
+        """
+        Slot connected to "MouseMoved" "qubImage" signal
+        To be reimplemented.
+        """
+        pass
+
+    def mousePress(self,  event):
+        """
+        Slot connected to "MousePressed" "qubImage" signal
+        To be reimplemented.
+        """
+        pass
+
+    def mouseRelease(self, event):
+        """
+        Slot connected to "MouseReleased" "qubImage" signal
+        To be reimplemented.
+        """
+        pass
+    
+    def viewportUpdate(self):
+        """ 
+        Slot connected to "ViewportUpdate" "qubImage" signal
+        To be reimplemented.
+        """
+        pass
+        
+###############################################################################
+####################            QubToggleImageAction        ###################
+###############################################################################
+class QubToggleImageAction(QubImageAction):
+    """
+    Base class for all action acting on image, representing by a toggle button
+    in the menubar of the QubView.
+    The name of the action will be used to find the icon file.
+    """
+    def __init__(self,*args, **keys):
+        """
+        Constructor method
+        name ... :  string name of the action
+        qubImage :  action will act on a QubImage object. It can be set in
+                    the constructor method or using the "viewConnect" method
+                    of the class 
+        place .. :  where to put in the view widget, the selection widget
+                    of the action ("toolbar", "statusbar", None)
+        show ... :  If in view toolbar, tells to put it in the toolbar
+                    itself or in the context menu
+        group .. :  actions may grouped. Tells the name of the group the
+                    action belongs to. If not present, a "misc." group is
+                    automatically created and the action is added to it
+        index .. :  Position of the selection widget of the action in its
+                    group
+        """
+        QubImageAction.__init__(self, *args, **keys)
+            
+    def addToolWidget(self, parent):
+        """
+        Creates action widget to put in "group" dockarea of the "toolbar"
+        Return this widget.
+        Use name of the action to find icon file
+        """
+        if self._widget is None:
+            self._widget = qt.QToolButton(parent, "%s"%self._name)
+            self._widget.setIconSet(qt.QIconSet(loadIcon("%s.png"%self._name)))
+            self._widget.setAutoRaise(True)
+            self._widget.setToggleButton(True)
+            self.connect(self._widget, qt.SIGNAL("toggled(bool)"),
+                         self.setState)
+            qt.QToolTip.add(self._widget, "%s"%self._name)
+        
+        return self._widget
+        
+    def addMenuWidget(self, menu):
+        """
+        This method should be reimplemented.
+        Creates item in contextmenu "menu" for the action
+        """
+        if self._item is None:
+            self._menu = menu
+            iconSet = qt.QIconSet(loadIcon("%s.png"%self._name))
+            self._item = menu.insertItem(iconSet, qt.QString(self._name),
+                                          self.menuChecked)
+
+    def addStatusWidget(self, parent):
+        """
+        Creates action widget to put in  the "statusbar"
+        Return this widget.
+        Use name of the action to find icon file
+        """
+        if self._widget is None:
+            self._widget = qt.QToolButton(parent, "%s"%self._name)
+            self._widget.setIconSet(qt.QIconSet(loadIcon("%s.png"%self._name)))
+            self._widget.setAutoRaise(True)
+            self._widget.setToggleButton(True)
+            self.connect(self._widget, qt.SIGNAL("toggled(bool)"),
+                         self.setState)
+            qt.QToolTip.add(self._widget, "%s"%self._name)
+        
+        return self._widget
+       
+    def menuChecked(self):
+        """
+        Slot connected to clicked() signal of menu item.
+        As there is no parameter on the state of the toglle menu item,
+        we build it and call the setState method of the class
+        """
+        if self._menu.isItemChecked(self._item):
+            checked = 0
+        else:
+            checked = 1
+        self.setState(checked)
+        
+            
+    def setState(self, state):
+        """
+        "state" is True or False.
+        Set toolbar, contextmenu or statusbar widgets of the action to
+        the "state" value.
+        Call the internal method _setState which is specific for each subclass
+        and which manage the behavior.
+        """
+        if self._widget is not None:
+            self._widget.setOn(state)
+        
+        if self._item is not None:
+            self._menu.setItemChecked(self._item, state)
+        
+        self._setState(state)
+        
+    def _setState(self, state):
+        """
+        Set action behavior to "state".
+        Should be reimplemented
+        """
+        pass
               
 ################################################################################
 ####################    TEST -- QubViewActionTest -- TEST   ####################
 ################################################################################
-from Qub.Widget.QubImageView import QubImageView
-from Qub.Print.QubPrintPreviewAction import QubPrintPreviewAction
-        
 class QubMain(qt.QMainWindow):
     def __init__(self, parent=None, file=None):
         qt.QMainWindow.__init__(self, parent)
@@ -200,6 +431,9 @@ class QubMain(qt.QMainWindow):
                
 ##  MAIN   
 if  __name__ == '__main__':
+    from Qub.Widget.QubImageView import QubImageView
+    from Qub.Print.QubPrintPreviewAction import QubPrintPreviewAction
+    
     app = qt.QApplication(sys.argv)
 
     qt.QObject.connect(app, qt.SIGNAL("lastWindowClosed()"),
