6f92307034145a1dc115e84b4000d3a4b15cf989
seb <seb@6dc7f912-7f33-0410-92c5-e0fef42ab755>
2006-07-27 07:00:27 +0000

    memory leak
    git-svn-id: https://scm.blissgarden.org/svn/qub/trunk@157 6dc7f912-7f33-0410-92c5-e0fef42ab755

--- Widget/QubOpenCv/QubOpenCv_qtTools.cpp
+++ Widget/QubOpenCv/QubOpenCv_qtTools.cpp
@@ -1,12 +1,16 @@
 #include <QubOpenCv_qtTools.h>
-
+//		      ####### Execption #######
+qtTools::Exception::Exception(const char *aMessage)
+{
+  strncpy(_Message,aMessage,sizeof(_Message));
+}
 static inline void _copy_opencv2qimage_3channel(const IplImage *aSImage,
-						QImage *aQImage)
+						QImage &aQImage)
 {
   for(int line = 0;line < aSImage->height;++line)
     {
       char *aPt = aSImage->imageData + line * aSImage->widthStep;
-      QRgb *aDestPt = (QRgb*)aQImage->scanLine(line);
+      QRgb *aDestPt = (QRgb*)aQImage.scanLine(line);
       for(int column = 0;column < aSImage->width;++column,aPt += 3,++aDestPt)
 	{
 	  int r = aPt[2],g = aPt[1],b = aPt[0];
@@ -16,12 +20,12 @@ static inline void _copy_opencv2qimage_3channel(const IplImage *aSImage,
 }
 
 static inline void _copy_opencv2qimage_3channel_aplha(const IplImage *aSImage,
-						      QImage *aQImage)
+						      QImage &aQImage)
 {
   for(int line = 0;line < aSImage->height;++line)
     {
       char *aPt = aSImage->imageData + line * aSImage->widthStep;
-      QRgb *aDestPt = (QRgb*)aQImage->scanLine(line);
+      QRgb *aDestPt = (QRgb*)aQImage.scanLine(line);
       for(int column = 0;column < aSImage->width;++column,aPt += 4,++aDestPt)
 	{
 	  int r = aPt[2],g = aPt[1],b = aPt[0],a = aPt[3];
@@ -30,23 +34,22 @@ static inline void _copy_opencv2qimage_3channel_aplha(const IplImage *aSImage,
     }    
 }
 
-QImage* qtTools::getQImageFromImageOpencv(const IplImage *aSImage)
+QImage qtTools::getQImageFromImageOpencv(const IplImage *aSImage) throw(qtTools::Exception)
 {
-  QImage *aQImage = NULL;
   if(aSImage && (aSImage->depth & IPL_DEPTH_8U))	// Only 8 bit Image
     {
       int depth = aSImage->nChannels == 1 ? 8 : 32;
-      aQImage = new QImage(aSImage->width,aSImage->height,depth);
+      QImage aQImage(aSImage->width,aSImage->height,depth);
       if(aSImage->nChannels == 1)
 	{
 	  int lines;
 	  char *aPt;
 	  for(lines = 0,aPt = aSImage->imageData;
 	      lines < aSImage->height;++lines,aPt += aSImage->widthStep)
-	    memcpy(aQImage->scanLine(lines),aPt,aSImage->width);
-	  aQImage->setNumColors(256);
+	    memcpy(aQImage.scanLine(lines),aPt,aSImage->width);
+	  aQImage.setNumColors(256);
 	  for(int i = 0;i < 256;++i)
-	    aQImage->setColor(i,qRgb(i,i,i));
+	    aQImage.setColor(i,qRgb(i,i,i));
 	}
       else
 	{
@@ -55,8 +58,9 @@ QImage* qtTools::getQImageFromImageOpencv(const IplImage *aSImage)
 	  else if(aSImage->nChannels == 4)
 	    _copy_opencv2qimage_3channel_aplha(aSImage,aQImage);
 	}
+      return aQImage;
     }
-  return aQImage;
+  else throw Exception("Only 8 bit Image can be handle");
 }
 
 static inline void _copy_qimage2opencv_3channel(const QImage *aQImage,
--- Widget/QubOpenCv/QubOpenCv_qtTools.h
+++ Widget/QubOpenCv/QubOpenCv_qtTools.h
@@ -1,10 +1,17 @@
 #include "qimage.h"
 #include "opencv/cxcore.h"
 
+
 class qtTools
 {
 public :
-  static QImage* getQImageFromImageOpencv(const IplImage*);
+  struct Exception
+  {
+    Exception(const char*);
+    char _Message[512];
+  };
+
+  static QImage getQImageFromImageOpencv(const IplImage*) throw(Exception);
   static IplImage* getImageOpencvFromQImage(const QImage*);
 };
 
