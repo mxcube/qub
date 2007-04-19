5a6560e700874c2661ef17bbcddee783caeb6274
seb <seb@6dc7f912-7f33-0410-92c5-e0fef42ab755>
2007-04-19 17:53:49 +0000

    *** empty log message ***
    git-svn-id: https://scm.blissgarden.org/svn/qub/trunk@248 6dc7f912-7f33-0410-92c5-e0fef42ab755

---
+++ CTools/pixmaptools/pixmaptools_lut.h
@@ -0,0 +1,74 @@
+#ifndef __PIXMAPTOOLS_LUT
+#define __PIXMAPTOOLS_LUT
+
+#include "qimage.h"
+
+class LutError
+{
+public:
+  LutError(const char * aMessage)
+  {
+    if(aMessage)
+      _msg = strdup(aMessage);
+    else
+      _msg = strdup("");
+  }
+  LutError(const LutError &aLutError)
+  {
+    if(aLutError._msg) 
+      _msg = strdup(aLutError._msg);
+    else
+      _msg = strdup("");
+  }
+  ~LutError() {free(_msg);}
+
+  const char* msg() const {return _msg;}
+private:
+  char *_msg;
+};
+
+class LUT
+{
+public :
+  enum mapping_meth {LINEAR,LOG};
+  struct XServer_Info;
+  
+public:
+  class Palette
+  {
+    friend class LUT;
+  public:
+    enum palette_type {GREYSCALE,TEMP,RED,GREEN,BLUE,REVERSEGREY,MANY,USER};
+    enum mode {RGBX,BGRX};
+    enum endian {LSB,MSB};
+
+    explicit Palette(palette_type = USER,mode = BGRX) throw();
+    
+    void setPaletteData(const unsigned int *aPaletteDataPt,int aSize) throw(LutError);
+    void getPaletteData(unsigned int* &aPaletteDataPt,int &aSize);
+    void fillPalette(palette_type) throw();
+    void fillSegment(int from,int to,
+		     double R1,double G1,double B1,
+		     double R2,double G2,double B2) throw(LutError);
+    void endFillSegment() throw();
+  private:
+    
+    void _fillSegment(const XServer_Info&,
+		      int from,int to,
+		      double R1,double G1,double B1,double R2,double G2,double B2) throw();
+    void _calcPalette(unsigned int palette[],int fmin, int fmax,mapping_meth) throw();
+
+    unsigned int _dataPalette[0x10000];
+    unsigned int _logdataPalette[0x10000];
+    mode         _mode;
+  };
+public:
+  template<class IN> static QImage map_on_min_max_val(const IN *data,int column,int row,Palette &aPalette,
+						      mapping_meth aMeth,
+						      IN &dataMin,IN &dataMax);
+
+  template<class IN> static QImage map(const IN *data,int column,int row,
+				       Palette&,mapping_meth,
+				       IN dataMin,IN dataMax);
+};
+#endif
