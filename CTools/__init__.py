f3004ef159ad17bad5ad891d98b5138e42157882
guijarro <guijarro@6dc7f912-7f33-0410-92c5-e0fef42ab755>
2007-11-27 15:32:42 +0000

    git-svn-id: https://scm.blissgarden.org/svn/qub/trunk@377 6dc7f912-7f33-0410-92c5-e0fef42ab755

--- CTools/__init__.py
+++ CTools/__init__.py
@@ -1,15 +1,19 @@
 import os
 import os.path
 
+dirname = __path__[0]
+platforms = []
+
 try:
-  __path__ = os.environ["QUB_COMPAT_OS"].split()
+  platforms = os.environ["QUB_COMPAT_OS"].split()
 except KeyError:
   cs_admin_path = '/csadmin/local/scripts/get_compat_os.share'
   if os.access(cs_admin_path,os.X_OK) :
-    dirname = __path__[0]
     cmd = os.popen(cs_admin_path)
-    for platform in cmd.read().split() :
-        __path__.append(os.path.join(dirname,'lib',platform))
+    platforms = cmd.read().split()
     cmd.close()
   else:
     raise ImportError('This module cannot be imported without get_compat_os.share, or define the QUB_COMPAT_OS environment variable.')
+
+for plat in platforms:
+    __path__.append(os.path.join(dirname,'lib',plat))
