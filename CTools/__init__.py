9d463b71329d4515bdcd316d8baa421c73ae3487
guijarro <guijarro@6dc7f912-7f33-0410-92c5-e0fef42ab755>
2007-11-27 15:19:54 +0000

    looks for QUB_COMPAT_OS to get compat os list, fallbacks to the compat_os.share script
    git-svn-id: https://scm.blissgarden.org/svn/qub/trunk@375 6dc7f912-7f33-0410-92c5-e0fef42ab755

--- CTools/__init__.py
+++ CTools/__init__.py
@@ -1,12 +1,15 @@
 import os
 import os.path
 
-cs_admin_path = '/csadmin/local/scripts/get_compat_os.share'
-if os.access(cs_admin_path,os.X_OK) :
+try:
+  __path__ = os.environ["QUB_COMPAT_OS"].split()
+except KeyError:
+  cs_admin_path = '/csadmin/local/scripts/get_compat_os.share'
+  if os.access(cs_admin_path,os.X_OK) :
     dirname = __path__[0]
     cmd = os.popen(cs_admin_path)
     for platform in cmd.read().split() :
         __path__.append(os.path.join(dirname,'lib',platform))
     cmd.close()
-else:
-    raise ImportError('This module can not be import without get_compat_os.share')
+  else:
+    raise ImportError('This module cannot be imported without get_compat_os.share, or define the QUB_COMPAT_OS environment variable.')
