16bb31120e1c7ee89a6c911c68a79277fe89d64f
Matias Guijarro <guijarro@ubguijarro.esrf.fr>
2010-12-09 14:55:06 +0100

    new setup.py

--- CTools/setup.py
+++ CTools/setup.py
@@ -1,8 +1,36 @@
 import glob
 import platform
 from distutils.core import setup,Extension
+import sipdistutils
 import numpy
 import os
+
+QT3=True
+if QT3:
+    print "***** doing setup.py for Qt3 *****"
+    import pyqtconfig
+else:
+    print "***** doing setup.py for Qt4 *****"
+    import PyQt4.pyqtconfig as pyqtconfig
+
+# Get the PyQt configuration information.
+config = pyqtconfig.Configuration()
+if QT3:
+  sip_flags = config.pyqt_qt_sip_flags.split()
+  qt_inc_dir = config.qt_inc_dir
+else:
+  sip_flags = config.pyqt_sip_flags
+
+# configure sip build_ext
+class my_own_build_ext(sipdistutils.build_ext):
+   def __init__(self, *args, **kwargs):
+     sipdistutils.build_ext.__init__(self, *args, **kwargs) 
+   def finalize_options(self):
+     sipdistutils.build_ext.finalize_options(self)
+     self.sip_opts = sip_flags
+   def _sip_sipfiles_dir(self):
+     config=pyqtconfig.Configuration()
+     return config.pyqt_sip_dir
 
 if platform.system() == 'Linux' :
     try:
@@ -42,7 +70,15 @@ mar345_module = Extension(name = "mar345",
                           extra_link_args = extra_link_args,
                           include_dirs = ['mar345',numpy.get_include()])
 
+pixmaptools_module = Extension(name = "pixmaptools",
+                               sources = ["pixmaptools/%s.sip" % (QT3 and "pixmaptools_qt3" or "pixmaptools_qt4")]+glob.glob('pixmaptools/pixmaptools_*.cpp'),
+                               extra_compile_args = extra_compile_args,
+                               extra_link_args = extra_link_args,
+                               include_dirs = ['pixmaptools', numpy.get_include(), qt_inc_dir]) 
+                               
+
 setup(name = "QubCTools",version = "1.0",
       description = "Some tools written in C for speed",
-      ext_modules = [polygone_module,dataresize_module,sps_module,mar345_module])
+      ext_modules = [polygone_module,dataresize_module,sps_module,mar345_module,pixmaptools_module],
+      cmdclass={'build_ext': my_own_build_ext})
 
